14937d2ba426b02913388e28fd3bc003bb406866
Ozioma Isaiah <oziomaisaiah235@gmail.com>
2025-11-24 18:15:14 +0100

    moved 'use client' to top of script

--- app/page.tsx
+++ app/page.tsx
@@ -65,8 +65,8 @@
 // }
 
 // At the top of your component
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 "use client";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 import { useState } from "react";
 
